58d03d26b6a5681d9445b40caf20c4bdee5dd375
Morning-Gloryy <lhlt452004@gmail.com>
2025-10-05 23:00:42 +0700

    track file

---
+++ openpose/models/getBaseModels.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8683602ae7a7f8cf049b25a6684ce833c1c381f460e49b5fc153d18a1f9f0e5
+size 1298
---
+++ openpose/models/getCOCO_and_MPII_optional.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3594c39348c3720262c7dcfc7ee0ea98cec70d4771e407d126061233acaa5a0d
+size 1049
